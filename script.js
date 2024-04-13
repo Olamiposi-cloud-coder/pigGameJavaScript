aa471d92436d2bc1192a17ab83d8a85a1de5e025
Olamiposi <olamiposicloud25@gmail.com>
2024-04-13 23:51:32 +0100

    progressing

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 'use strict';
 
 const player = document.querySelectorAll('.player');
-const 
+const firstScoreEl = document.querySelector('#score--0');
+const secondSCoreEl = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+firstScoreEl.textContent = 0;
+secondSCoreEl.textContent = 0;
+diceEl.classList.toggle('hidden');
