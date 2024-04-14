e7d21ff25bfe7976b075449c3f31a67e4784be26
Olamiposi <olamiposicloud25@gmail.com>
2024-04-14 15:25:26 +0100

    working on the js

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const player = document.querySelectorAll('.player');
 const firstScoreEl = document.querySelector('#score--0');
 const secondSCoreEl = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const newButtonEl = document.querySelector('.btn-new');
+const rollButtonEl = document.querySelector('.btn-roll');
+const holdButtonEl = document.querySelector('.btn-hold');
 
 firstScoreEl.textContent = 0;
 secondSCoreEl.textContent = 0;
